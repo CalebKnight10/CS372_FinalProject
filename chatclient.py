cd63edba981518b12e1e21a858b8d774901492e2
Caleb Knight <knightca@oregonstate.edu>
2022-12-03 12:19:00 -0800

    Added imports, and comments giving me a basic idea of how to start

---
+++ chatclient.py
@@ -0,0 +1,28 @@
+import sys
+import json
+import socket
+import threading
+from chatui import init_windows, read_command, print_message, end_windows
+
+
+# Need to get the hello payload
+
+
+
+# Need to get the clients chat payload
+
+
+
+# Use init_windows
+
+
+
+# Connect to socket
+
+
+
+# Get hello 
+
+
+
+# Get the client chat
---
+++ chatserver.py
@@ -0,0 +1,44 @@
+import sys
+import socket
+import threading
+import json
+import select
+
+
+
+
+# Have dict/set for sockets/buffers
+
+
+
+# Handle listener socket
+
+
+
+
+# Connect socket
+
+
+
+
+# If socket is listener, accept
+
+# Else, recv data if no data close
+
+
+
+
+# Get the servers chat payload
+
+
+
+
+
+# Get the join and the leave payload
+
+
+
+
+
+
+
