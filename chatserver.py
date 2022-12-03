576578597ba5e379d539a33c0379a7816a027b46
Caleb Knight <knightca@oregonstate.edu>
2022-12-03 14:01:04 -0800

    Used project description to make functions for the server chat, joining person, and leaving person's payloads

--- chatserver.py
+++ chatserver.py
@@ -17,7 +17,12 @@ import select
 
 
 # Connect socket
-
+# Make the server socket and bind it to the port
+servers_socket = socket.socket()
+servers_socket.bind(("", port))
+servers_socket.liste
+# listener socket
+listener = [servers_socket]
 
 
 
@@ -29,16 +34,27 @@ import select
 
 
 # Get the servers chat payload
-
-
-
+def get_server_chat_payload(nickname, chat):
+    server_chat = {
+    "type": "chat",
+    "nick": nickname,
+    "message": chat
+    }
+    return json.dumps(server_chat)
 
 
 # Get the join and the leave payload
+def get_join_payload(name_of_joinee):
+    join = {
+    "type": "join",
+    "nick": name_of_joinee
+    }
+    return json.dumps(join)
 
-
-
-
-
-
+def get_leave_payload(name_of_leaver):
+    leave = {
+    "type": "leave",
+    "nick": name_of_leaver
+    }
+    return json.dumps(leave)
 
