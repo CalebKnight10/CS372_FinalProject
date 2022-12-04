afae76f5b8180d853200caaa2d867305a43eef77
Caleb Knight <knightca@oregonstate.edu>
2022-12-04 14:00:01 -0800

    Got a function to check the contents of a packet on the client side, another to handle the server contents, and another to work with user input.  Still not working but will come back soon

--- chatclient.py
+++ chatclient.py
@@ -10,52 +10,71 @@ from chatui import init_windows, read_command, print_message, end_windows
 
 
 # Need to get the hello payload
-def helloPayload(nick):
-	message = {
+def helloPayload(nickname):
+	contents = {
         "type": "hello",
         "nick": nickname
     }
-	message = json.dumps(message)
-    
-    return message
+	contents = json.dumps(contents)\
 
-def userInput(nick):
-	while True:
-        try:
-            command = read_command(f"{nickname}>  ")
-        except:
-            break
-        print_message(f"{nick}: {command}")	
+	return contents
+
 
 
 # Need to get the clients chat payload
-def getClientsChatPayload(message):
+def clientsChatPayload(contents):
     client_chat_payload = {
     "type": "chat",
-    "message": message
+    "message": contents
     }
+    contents = json.dumps(client_chat_payload)
 
-    return json.dumps(client_chat_payload)
+    return contents
+
+
+
+
+def contentsOfPacket(contents):
+	if contents['type'] == 'join':
+		print(f"*** {packet['nickname']} joined the chat")
+	if contents['type'] == 'chat':
+		print(f"{packet['nickname']} {packet['contents']}")
+	else:
+		print(f"*** {packet['nickname']} left the chat")
+
+def serverContents(s):
+	while True:
+		contents = s.recv(4096).decode()
+		contents = json.load(contents)
+		print_message(contentsOfPacket(contents))
+
+def userInput(nickname, s):
+	while True:
+		contents = read_command(f"{nickname}>  ")
+		print_message(f"{nick}: {contents}")
+		if contents[0] == '/q':
+			s.close()
+		elif len(contents) == 0:
+			continue
+		else:
+			s.sendall(getClientsChatPayload(contents).encode())
 
 
 def main(argv):
+	nickname = argv[1]
+	port =int(argv[2])
+	host = argv[3]
 
-    try:
-        nickname = argv[1]
-        port =int(argv[2])
-        host = argv[3]
-        
-    init_windows()
+	init_windows()
 
-    # Connect to socket
+	# Connect to socket
 	s = socket.socket()
-    s.connect((host, port))
+	s.connect((host, port))
 
-    hello_message = helloPayload(nick)
-    s.sendall(hello_message)
-
-
-    s.close()
+	hello_message = helloPayload(nick)
+	s.sendall(hello_message)
+	
+	s.close()
 
 
 if __name__ == "__main__":
