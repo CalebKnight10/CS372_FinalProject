07c1e837f5ba98d1795f3fc48bccf71f38b6d136
Caleb Knight <knightca@oregonstate.edu>
2022-12-04 13:26:58 -0800

    Adapted my select() project to fit our case.  Added in a function to handle a join packet or a chat packet, leave packet is the conditional to check.

--- chatclient.py
+++ chatclient.py
@@ -1,3 +1,7 @@
+# Caleb Knight
+# CS 372 Final Project
+# A client chat server
+
 import sys
 import json
 import socket
@@ -6,23 +10,56 @@ from chatui import init_windows, read_command, print_message, end_windows
 
 
 # Need to get the hello payload
+def helloPayload(nick):
+	message = {
+        "type": "hello",
+        "nick": nickname
+    }
+	message = json.dumps(message)
+    
+    return message
 
+def userInput(nick):
+	while True:
+        try:
+            command = read_command(f"{nickname}>  ")
+        except:
+            break
+        print_message(f"{nick}: {command}")	
 
 
 # Need to get the clients chat payload
+def getClientsChatPayload(message):
+    client_chat_payload = {
+    "type": "chat",
+    "message": message
+    }
+
+    return json.dumps(client_chat_payload)
+
+
+def main(argv):
+
+    try:
+        nickname = argv[1]
+        port =int(argv[2])
+        host = argv[3]
+        
+    init_windows()
+
+    # Connect to socket
+	s = socket.socket()
+    s.connect((host, port))
+
+    hello_message = helloPayload(nick)
+    s.sendall(hello_message)
+
+
+    s.close()
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
 
 
 
-# Use init_windows
-
-
-
-# Connect to socket
-
-
-
-# Get hello 
-
-
-
-# Get the client chat
--- chatserver.py
+++ chatserver.py
@@ -1,3 +1,8 @@
+# Caleb Knight
+# CS 372 Final Project
+# A client chat server
+
+
 import sys
 import socket
 import threading
@@ -5,36 +10,8 @@ import json
 import select
 
 
-
-
-# Have dict/set for sockets/buffers
-
-
-
-# Handle listener socket
-
-
-
-
-# Connect socket
-# Make the server socket and bind it to the port
-servers_socket = socket.socket()
-servers_socket.bind(("", port))
-servers_socket.liste
-# listener socket
-listener = [servers_socket]
-
-
-
-# If socket is listener, accept
-
-# Else, recv data if no data close
-
-
-
-
 # Get the servers chat payload
-def get_server_chat_payload(nickname, chat):
+def getServerChatPayload(nickname, chat):
     server_chat = {
     "type": "chat",
     "nick": nickname,
@@ -44,17 +21,95 @@ def get_server_chat_payload(nickname, chat):
 
 
 # Get the join and the leave payload
-def get_join_payload(name_of_joinee):
+def getJoinPayload(name_of_joinee):
     join = {
     "type": "join",
     "nick": name_of_joinee
     }
     return json.dumps(join)
 
-def get_leave_payload(name_of_leaver):
+def getLeavePayload(name_of_leaver):
     leave = {
     "type": "leave",
     "nick": name_of_leaver
     }
     return json.dumps(leave)
 
+
+
+def runServer(port):
+	# Have dict/set for sockets/buffers
+    client_buffers = {}
+
+    # Make the server socket and bind it to the port
+    servers_socket = socket.socket()
+    servers_socket.bind(("", port))
+    servers_socket.listen()
+
+    # listener socket
+    listener = [servers_socket]
+
+    # loop forever
+    while True:
+
+        # call select(), get sockets that are ready-to-read
+        ready_to_read, _, _ = select.select(listener, [], [])
+
+        # for each socket that is ready-to-read
+        for s in ready_to_read:
+
+            # if the socket is the listener socket, then accept the connection
+            if s is servers_socket:
+                clients_socket, _ = servers_socket.accept()
+                listener.append(clients_socket)
+
+            # else it is just a regular socket
+            else:
+                contents = s.recv(4096)
+
+                # if no content, client disconnected
+                if not contents:
+                    print_client_disconnection(s)
+                    s.close()
+                    listener.remove(s)
+                    client = client_buffers.pop(s)
+                    getLeavePayload(client)
+                    print(f"*** {client} left the chat")
+                else:
+                	for c in contents.decode():
+                		if re.search('join', c):
+                			joinPacket(s, contents, client_buffers)
+                		else:
+                			chatPacket(s, contents, client_buffers)
+
+
+
+def joinPacket(s, contents, client_buffers): 
+	contents = contents.decode()
+
+	join_packet = json.load(contents)
+	print(f"*** {join_packet['nickname']} joined the chat")
+	client_buffers[s] = join_packet['nickname']
+	getJoinPayload(join_packet['nickname'])
+
+
+def chatPacket(s, contents, client_buffers):
+	contents = contents.decode()
+
+	chat_packet = json.load(contents)
+
+	print(f"*** {join_packet['nickname']} joined the chat")
+	client_buffers[s] = join_packet['nickname']
+	getServerChatPayload(join_packet['nickname'])
+
+def main(argv):
+
+	port = int(argv[1])
+
+	runServer(port)
+
+
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
